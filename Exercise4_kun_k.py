bdb306197b3d7d7dda24b7c4f67be04d1c44038b
august2011 <74868673+august2011@users.noreply.github.com>
2021-06-09 16:50:33 +0700

    Add files via upload

---
+++ Exercise4_kun_k.py
@@ -0,0 +1,10 @@
+Foundation_English = 80
+General_Business = 75
+IntroductionToComputerSystems = 85.5
+ComputerProgramming = 78.5
+
+print("---Your Score---")
+print("Foundation English :",Foundation_English)
+print("General Business :",General_Business)
+print("Introduction to Computer System :",IntroductionToComputerSystems)
+print("Computer Programming :",ComputerProgramming)
